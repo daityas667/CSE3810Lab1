46e384257b66bf75b55c8f16e37dd6a3d9eecd0d
daityas667 <57953914+daityas667@users.noreply.github.com>
2020-08-25 13:00:07 -0400

    Update sender.py

--- sender.py
+++ sender.py
@@ -1,9 +1,26 @@
+import random
 from scapy.all import *
 
 conf.iface="wlan0"
 
 dst = "192.168.1.100"
 msg = "adastra per explotium"
+
+random.seed()
+msg_segs = []
+seg = []
+
+for char in msg:
+    seg.append(char)
+    flag = random.randint(1, 3)
+    if flag == 3:
+        msg_segs.append(seg)
+        seg = []
+
+seg = []
+for seg in msg_segs:
+    seg.insert(0,chr(171))
+    seg.insert(0,chr(172))
 
 pkt = IP(dst=dst)/ICMP()/Raw(load=msg)
 send(pkt)

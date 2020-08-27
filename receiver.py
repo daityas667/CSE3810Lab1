5231f0c1803b01361a4ae8d159d1f3ea1cbaa33b
Isaiah <60484669+IsaiahST2020@users.noreply.github.com>
2020-08-27 06:52:25 -0500

    Update receiver.py

--- receiver.py
+++ receiver.py
@@ -1,11 +1,12 @@
 from scapy.all import *
-
+  
 def pkt_callback(pkt):
     if pkt.haslayer("TCP") and pkt.haslayer("Raw"):
         msg = str(pkt[Raw].load)
-        if msg[0] == chr(171) or msg[1] == chr(171):
+        if msg[0] >= chr(171) and msg[0] <= chr(180):
             print(str(pkt[Raw].load))
 
 print("[+] Started Listener")
 
-sniff(iface="ens4",prn=pkt_callback,filter='host 10.128.15.239',store=0)
+sniff(iface="ens4",prn=pkt_callback,filter='host 10.128.15.244',store=0)
+#host ip = my internal IP

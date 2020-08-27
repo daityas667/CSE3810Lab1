1be146bd851699253b45f405dc5226686bfc9626
Isaiah <60484669+IsaiahST2020@users.noreply.github.com>
2020-08-26 21:15:19 -0500

    Update send.py

--- send.py
+++ send.py
@@ -1,15 +1,13 @@
 import random
 from scapy.all import *
-
 conf.iface="ens4"
 
-dest = str(sys.argv[1])
-print(dest)
+src = "10.128.15.244" #internal ip "35.239.112.246"
+dest = "35.239.112.246" #external ip "10.128.15.244"
 
 msg = "adastra per explotium"
-
 def send_packet(seg):
-        pkt = IP(dst=dest)/TCP()/Raw(load=seg)
+        pkt = IP(src=src,  dst=dest)/TCP()/Raw(load=seg)
         send(pkt)
 
 def main():
@@ -19,26 +17,22 @@ def main():
         num_sent = 0
         sent = False
 
-        while(num_sent < needed):
-                to_send = msg[sent]
+        while(num_sent < needed - 1):
+                to_send = msg[num_sent]
                 while(sent == False):
                         flag = random.randint(1,4)
                         if flag == 4:
                                 to_send = to_send + chr(171)
-                                to_send = to_send + chr(172)
                                 send_packet(to_send)
                                 print("[+] Sent packet containing portion of message")
                                 sent = True
+                                num_sent += 1
                         else:
                                 pkt = IP(dst=dest)/TCP()
                                 send(pkt)
                                 print("[+] Sent junk packet")
                                 sent = False
-
-        to_send = ""
-        to_send.append(char(171))
-        to_send.append(char(171))
-        send_packet(to_send) #Sent packet indacating transmission has completed
+                sent = False
         print("[+] Done sending message")
 
 main()

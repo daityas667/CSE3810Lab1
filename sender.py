767fc5ee92fb56391f63ddeb1cdd550cb694a8b8
Isaiah <60484669+IsaiahST2020@users.noreply.github.com>
2020-08-27 06:51:36 -0500

    Update sender.py
    updated

--- sender.py
+++ sender.py
@@ -5,7 +5,7 @@ conf.iface="ens4"
 src = "10.128.15.244" #internal ip
 dest = "35.239.112.246" #external ip
 
-msg = "adastra per explotium"
+msg = "Meet me in CSE-3801 tomorrow"
 def send_packet(seg):
         pkt = IP(src=src,  dst=dest)/TCP()/Raw(load=seg)
         send(pkt)
@@ -22,7 +22,8 @@ def main():
                 while(sent == False):
                         flag = random.randint(1,4)
                         if flag == 4:
-                                to_send = chr(171) + to_send
+                                indic = random.randint(1,10)
+                                to_send = chr(170 + indic) + to_send
                                 send_packet(to_send)
                                 print("[+] Sent packet containing portion of message")
                                 sent = True

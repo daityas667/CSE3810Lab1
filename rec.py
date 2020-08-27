f74fbdd8c1b9b6aa46c29ea11d0a9ae4e0bd5726
daityas667 <57953914+daityas667@users.noreply.github.com>
2020-08-26 20:20:31 -0400

    Update rec.py

--- rec.py
+++ rec.py
@@ -3,20 +3,17 @@ from scapy.all import *
 final_msg = []
 
 def pkt_callback(pkt):
-    if pkt.haslayer("TCP"):
-        if pkt.haslayer("Raw"):
-            msg = str(pkt[Raw].load)
-			if msg[0] == chr(171) and msg[1] == chr(172): #Rec'd packet contains data from covert message
-				print("[+] Received portion of covert message")
-				final_msg.append(msg[3])
-			elif msg[0] == chr(171) and msg[1] == chr(171):
-				stdout.write("[+] Final message is :")
-				print(final_msg)
-				exit
-			else:
-				print("[+] Received junk packet from sender")
-				
+        if pkt.haslayer("TCP"):
+                if pkt.haslayer("Raw"):
+                        msg = str(pkt[Raw].load)
+                        if msg[0] == chr(171) and msg[1] == chr(172):
+                                print("[+] Received portion of covert message")
+                                final_msg.append(msg[3])
+                        elif msg[0] == chr(171) and msg[1] == chr(171):
+                                stdout.write("[+] Final message is :")
+                                print(final_msg)
+                                exit
 
 print("[+] Started Listener")
 
-sniff(iface="wlan0",prn=pkt_callback,filter='host 10.128.15.234',store=0)
+sniff(iface="ens4",prn=pkt_callback,filter='host 10.128.15.245',store=0)
